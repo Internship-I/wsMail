0bafb3bc4648ecd2ddc1496c917f6023a7452eee
nida sakinah aulia <nidasakinaaulia@gmail.com>
2025-10-02 15:38:06 +0700

    SECOND TRY

--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/gofiber/fiber/v2 v2.52.9
 	github.com/whatsauth/whatsauth v0.2.9
 	go.mau.fi/whatsmeow v0.0.0-20250930215512-38f9aaa3ba7c
+	go.mongodb.org/mongo-driver v1.17.0
 )
 
 require (
@@ -38,9 +39,7 @@ require (
 	github.com/microsoft/go-mssqldb v1.0.0 // indirect
 	github.com/montanaflynn/stats v0.7.1 // indirect
 	github.com/petermattis/goid v0.0.0-20250904145737-900bdf8bb490 // indirect
-	github.com/pkg/errors v0.9.1 // indirect
 	github.com/rivo/uniseg v0.4.4 // indirect
-	github.com/rogpeppe/go-internal v1.14.1 // indirect
 	github.com/rs/zerolog v1.34.0 // indirect
 	github.com/savsgio/gotils v0.0.0-20230208104028-c358bd845dee // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
@@ -51,10 +50,9 @@ require (
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
 	github.com/xdg-go/scram v1.1.2 // indirect
 	github.com/xdg-go/stringprep v1.0.4 // indirect
-	github.com/youmark/pkcs8 v0.0.0-20201027041543-1326539a0a0a // indirect
+	github.com/youmark/pkcs8 v0.0.0-20240726163527-a2c0da244d78 // indirect
 	go.mau.fi/libsignal v0.2.0 // indirect
 	go.mau.fi/util v0.9.1 // indirect
-	go.mongodb.org/mongo-driver v1.11.6 // indirect
 	golang.org/x/crypto v0.42.0 // indirect
 	golang.org/x/exp v0.0.0-20250911091902-df9299821621 // indirect
 	golang.org/x/net v0.44.0 // indirect
--- main.go
+++ main.go
@@ -1,19 +1,19 @@
 package main
 
 import (
-	// "log"
+	"context"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
+	"time"
 
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	// "github.com/Internship1/wsMail/config"
-
 	// "github.com/aiteung/musik"
 	// "github.com/gofiber/fiber/v2/middleware/cors"
-
-
 	// "github.com/Internship1/wsMail/url"
-
 	// "github.com/gofiber/fiber/v2"
 )
 
@@ -24,15 +24,42 @@ import (
 // 	log.Fatal(site.Listen(musik.Dangdut()))
 // }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintln(w, "Hello, World!")
-}
+// func handler(w http.ResponseWriter, r *http.Request) {
+//     fmt.Fprintln(w, "Hello, World!")
+// }
 
 func main() {
+    // Ambil connection string dari env
+    mongoURI := os.Getenv("MONGO_URI")
+    if mongoURI == "" {
+        log.Fatal("MONGO_URI not set")
+    }
+
+    // Connect MongoDB
+    clientOptions := options.Client().ApplyURI(mongoURI)
+    ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+    defer cancel()
+    client, err := mongo.Connect(ctx, clientOptions)
+    if err != nil {
+        log.Fatal("MongoDB connection error:", err)
+    }
+
+    if err := client.Ping(ctx, nil); err != nil {
+        log.Fatal("MongoDB ping failed:", err)
+    }
+
+    fmt.Println("Connected to MongoDB!")
+
+    // Simple HTTP server
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, "Hello, World!")
+    })
+
     port := os.Getenv("PORT")
     if port == "" {
         port = "8080"
     }
-    http.HandleFunc("/", handler)
-    http.ListenAndServe(":"+port, nil)
+
+    log.Println("Listening on port", port)
+    log.Fatal(http.ListenAndServe(":"+port, nil))
 }
