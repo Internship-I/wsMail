31c8707b590a424e8930331249ce4b32c9352098
nida sakinah aulia <nidasakinaaulia@gmail.com>
2025-10-08 11:46:04 +0700

    try to fix again

--- controller/struct.go
+++ controller/struct.go
@@ -5,18 +5,18 @@ import (
 )
 
 type Transaction struct {
-	ID             	primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
-	ConsigmentNote 	string             `bson:"consignment_note,omitempty" json:"consignment_note,omitempty"`
-	SenderName     	string             `bson:"sender_name,omitempty" json:"sender_name,omitempty"`
-	SenderPhone    	string             `bson:"sender_phone,omitempty" json:"sender_phone,omitempty"`
-	ReceiverName   	string             `bson:"receiver_name,omitempty" json:"receiver_name,omitempty"`
-	AddressReceiver string 			   `bson:"address_receiver,omitempty" json:"address_receiver,omitempty"`
+	ID              primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
+	ConsignmentNote string             `bson:"consignment_note,omitempty" json:"consignment_note,omitempty"`
+	SenderName      string             `bson:"sender_name,omitempty" json:"sender_name,omitempty"`
+	SenderPhone     string             `bson:"sender_phone,omitempty" json:"sender_phone,omitempty"`
+	ReceiverName    string             `bson:"receiver_name,omitempty" json:"receiver_name,omitempty"`
+	AddressReceiver string             `bson:"address_receiver,omitempty" json:"address_receiver,omitempty"`
 	ReceiverPhone   string             `bson:"receiver_phone,omitempty" json:"receiver_phone,omitempty"`
-	ItemContent    	string             `bson:"item_content,omitempty" json:"item_content,omitempty"`
-	DeliveryStatus 	string             `bson:"delivery_status,omitempty" json:"delivery_status,omitempty"`
-	CODValue       	float64            `bson:"cod_value,omitempty" json:"cod_value,omitempty"`
-	CreatedAt      	primitive.DateTime `bson:"created_at,omitempty" json:"created_at,omitempty"`
-	UpdatedAt      	primitive.DateTime `bson:"updated_at,omitempty" json:"updated_at,omitempty"`
+	ItemContent     string             `bson:"item_content,omitempty" json:"item_content,omitempty"`
+	DeliveryStatus  string             `bson:"delivery_status,omitempty" json:"delivery_status,omitempty"`
+	CODValue        float64            `bson:"cod_value,omitempty" json:"cod_value,omitempty"`
+	CreatedAt       primitive.DateTime `bson:"created_at,omitempty" json:"created_at,omitempty"`
+	UpdatedAt       primitive.DateTime `bson:"updated_at,omitempty" json:"updated_at,omitempty"`
 }
 
 type User struct {
@@ -28,19 +28,19 @@ type User struct {
 	Role                   string             `bson:"role,omitempty" json:"role,omitempty"` // "admin" or "kurir"
 }
 
-type ReqTransactionTransaction struct {
-	ID             	primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
-	ConsigmentNote 	string             `bson:"connote,omitempty" json:"connote,omitempty"`
-	SenderName     	string             `bson:"sender_name,omitempty" json:"sender_name,omitempty"`
-	SenderPhone    	string             `bson:"sender_phone,omitempty" json:"sender_phone,omitempty"`
-	ReceiverName   	string             `bson:"receiver_name,omitempty" json:"receiver_name,omitempty"`
-	AddressReceiver string 			   `bson:"address_receiver,omitempty" json:"address_receiver,omitempty"`
+type ReqTransaction struct {
+	ID              primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
+	ConsignmentNote string             `bson:"consignment_note,omitempty" json:"consignment_note,omitempty"`
+	SenderName      string             `bson:"sender_name,omitempty" json:"sender_name,omitempty"`
+	SenderPhone     string             `bson:"sender_phone,omitempty" json:"sender_phone,omitempty"`
+	ReceiverName    string             `bson:"receiver_name,omitempty" json:"receiver_name,omitempty"`
+	AddressReceiver string             `bson:"address_receiver,omitempty" json:"address_receiver,omitempty"`
 	ReceiverPhone   string             `bson:"receiver_phone,omitempty" json:"receiver_phone,omitempty"`
-	ItemContent    	string             `bson:"item_content,omitempty" json:"item_content,omitempty"`
-	DeliveryStatus 	string             `bson:"delivery_status,omitempty" json:"delivery_status,omitempty"`
-	CODValue       	float64            `bson:"cod_value,omitempty" json:"cod_value,omitempty"`
-	CreatedAt      	primitive.DateTime `bson:"created_at,omitempty" json:"created_at,omitempty"`
-	UpdatedAt      	primitive.DateTime `bson:"updated_at,omitempty" json:"updated_at,omitempty"`
+	ItemContent     string             `bson:"item_content,omitempty" json:"item_content,omitempty"`
+	DeliveryStatus  string             `bson:"delivery_status,omitempty" json:"delivery_status,omitempty"`
+	CODValue        float64            `bson:"cod_value,omitempty" json:"cod_value,omitempty"`
+	CreatedAt       primitive.DateTime `bson:"created_at,omitempty" json:"created_at,omitempty"`
+	UpdatedAt       primitive.DateTime `bson:"updated_at,omitempty" json:"updated_at,omitempty"`
 }
 
 type ReqUser struct {
