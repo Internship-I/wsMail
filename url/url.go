62c131b14c112e2f26869a04e7cc8d38efb0ea1b
nida sakinah aulia <nidasakinaaulia@gmail.com>
2025-10-09 09:27:40 +0700

    heroku push

--- url/url.go
+++ url/url.go
@@ -10,8 +10,9 @@ import (
 func Web(page *fiber.App) {
 	// page.Post("/api/whatsauth/request", controller.PostWhatsAuthRequest)  //API from user whatsapp message from iteung gowa
 	// page.Get("/ws/whatsauth/qr", websocket.New(controller.WsWhatsAuthQR)) //websocket whatsauth
-	page.Get("/checkip", controller.Homepage) //ujicoba panggil package musik
-	
+	// page.Get("/checkip", controller.Homepage) //ujicoba panggil package musik
+	page.All("/", controller.Sink)
+
 	page.Get("/", controller.Sink)
 	page.Post("/", controller.Sink)
 	page.Put("/", controller.Sink)
